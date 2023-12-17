d17e20a2e5794e75d4d08ed1811fa34f168a1227
mateen27 <khanmateen2713@gmail.com>
2023-12-17 15:09:48 +0530

    feat: Schema and Models with mongoose

---
+++ backend/models/userModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    pic: {
+      type: String,
+      required: true,
+      default:
+        "https://i.pinimg.com/564x/55/32/21/55322103501339cc4177683e55ad3dec.jpg",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.Model("User", userSchema);
+
+module.exports = User;
