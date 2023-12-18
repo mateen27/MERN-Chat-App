c5aab34c66c35f6e59713e42cad1fce9ed4cca88
mateen27 <khanmateen2713@gmail.com>
2023-12-18 06:46:14 +0530

    feat: connect MongoDB to NodeJS

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,28 @@
-const express = require('express');
-const { chats } = require('./data/data');
-require('dotenv').config()
+const express = require("express");
+const { chats } = require("./data/data");
+const connectDB = require("./config/db");
+const colors = require("colors");
+require("dotenv").config();
+
+// connecting to database
+connectDB();
 
 const app = express();
 
-app.get('/' , (req , res) => {
-    res.send("ApI is running");
-})
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
 
-app.get('/api/chat' , (req , res) => {
-    res.send(chats)
-})
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
 
-app.get('/api/chat/:id' , (req , res) => {
-    // finding the chat from the requested paramas by the user
-    const singleChat = chats.find( c => c._id === req.params.id )
-    res.send(singleChat)
-})
+app.get("/api/chat/:id", (req, res) => {
+  // finding the chat from the requested paramas by the user
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
 
-const PORT = process.env.PORT || 8081
+const PORT = process.env.PORT || 8081;
 
-app.listen(PORT , console.log(`Server started on ${PORT} Port`))
+app.listen(PORT, console.log(`Server started on ${PORT} Port`.yellow.bold));
